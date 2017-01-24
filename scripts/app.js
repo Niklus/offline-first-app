3e8b1dc3ff80a7637b25f290173ea34a9d934155
Nicholas <niklasoti@gmail.com>
2017-01-24 19:37:18 +0100

    Update

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 // Init Database
 
 var sourceStore = localforage.createInstance({
-	name: "sourrces"
+	name: "sources"
 });
 
 var articleStore = localforage.createInstance({
@@ -15,6 +15,7 @@ var articleStore = localforage.createInstance({
 var app = new Vue({
 	
 	el: '#app',
+	
 	data: {
 		
 		title: 'online',  //Use togle icon	
@@ -23,52 +24,127 @@ var app = new Vue({
 		
 		key: 'f6448decf20240cda23e0d6d98d31e9c',
 		
-		categories: [
-			{ type: 'all' },
-      		{ type: 'business' },
-      		{ type: 'gaming' },
-      		{ type: 'general' },
-      		{ type: 'music' },
-      		{ type: 'science-and-nature' },
-      		{ type: 'sport' },
-      		{ type: 'technology' }
+		categories: [ // ability to add categories
+			'all',
+			'business',
+			'gaming',
+			'general',
+			'music',
+			'science-and-nature',
+      		'sport',
+      		'technology'
 		],
 
-		results: []
+		results: [],
+
+		logos: [{
+			id: '',
+            size: {
+            	large: '',
+            	medium: '',
+            	small: ''
+            }
+		}],
+
+
+		currentCategory: '',
 	},
 
 	methods: {
+
+		setIcons: function(){
+
+			var imgs = this.imgs;
+			var self = this;
+
+			sourceStore.getItem(this.currentCategory)
+			.then(function(value) {		
+  				value.sources.forEach(function(obj){
+                    
+                    var id = obj.id;
+
+                    self.logos.push({
+                    	id: id,
+                    	size: obj.urlsToLogos
+                    });
+ 
+  				});
+  			});	
+		},
+
+		getIcons: function(id){
+
+			var arr = this.logos;
+
+			for(var i = 0; i<arr.length; i++){
+				if (arr[i].id === id){
+					return arr[i].size.small;
+				}
+			}
+		},
+
+		getImages: function(id){
+
+			console.log(this.imgs.id)
+		},
 
 		updateSources: function(){
 			// User can update sources whilst online
 			// run getSources iregadless of idb and (update idb)
 		},
 
+		getHeadlines: function(event){ 
 
-		getSources: function(event){ 
-
-            this.results.length = 0;
-
-           //1. Check if sources Exist in idb if(idb.category.length !== 0 then check)
-			//If they do, get articles direct without fetch
-			/*sources.forEach(function(obj){
-				this.getArticles(obj.id);
-			});
-			   return; //Dont run
-			*/
+			if (!navigator.onLine) {
+				console.log('offline');
+                 //Define offline behvior
+                 //Set categories
+                 //what to fetch e.t.c
+				//use idb
+				return;
+			}
 
 			//var lng: en, de, fr;
 			//var ctry: au, de, gb, in, it, us;
-
-			if(event.target.id !== 'all'){
-			   var ctgry = event.target.id;
-			   var url = 'https://newsapi.org/v1/sources?&category='+ctgry+'&apiKey='+this.key;
-			}else{
+            var ctgry;
+            var url;
+        
+			if(event.target.id == 'all'){
+			    ctgry = event.target.id;
 				url = 'https://newsapi.org/v1/sources?&apiKey='+this.key;
+			}else{   
+				ctgry = event.target.id;
+			    url = 'https://newsapi.org/v1/sources?&category='+ctgry+'&apiKey='+this.key;
 			}
 
-			var self = this; // Learn how to use call, apply and bind
-           
+
+			this.results.length = 0; 
+			//Clear if online
+            //Or if idb has data
+            //if idb empty and offline save current results to idb -just incase :)
+            
+            var self = this;
+
+  			sourceStore.getItem(ctgry).then(function(value) {
+  				
+  				if(!value){
+  					self.fetchSource(url, ctgry)
+  				}else{
+  					value.sources.forEach(function(obj){
+						self.getArticles(obj.id); //if a category returns null refresh sources/ delete category
+					});
+  				}
+	  			
+  			});
+
+
+  			this.currentCategory = ctgry;
+  			this.setIcons(); 
+        },
+
+        fetchSource:function(url, ctgry){
+            
+            var self = this; // Learn how to use call, apply and bind
 
 			fetch(url, {
 				method: 'get'
@@ -76,48 +152,48 @@ var app = new Vue({
 				return res.json();
 			}).then(function(res){
 
-			   res.sources.forEach(function(obj,i){
-					self.getArticles(obj.id, null, i);
+				sourceStore.setItem(ctgry, res);
+				console.log(ctgry + " saved!");
+
+			    res.sources.forEach(function(obj){
+					
+					self.getArticles(obj.id);
 				});
 
-				
-                 
+				 //Next time we dont need to make another request
+                
+                
+
                 //To-do here
 				//Save the array of sources in indexeddb in an object-category 
 			    //for quicker access next time?? e.g object music={sources : res} So we fetch articles directly
 			    //id, name, and image blobs
-			});		
+			});	
 		},
 
-		getArticles: function(source, sortBy, i){
+		getArticles: function(source, sortBy){
 
 			if(sortBy) {	
 				var url = 'https://newsapi.org/v1/articles?source='+source+'&sortBy='+sortBy+'&apiKey='+this.key;
 			}else {
 				url = 'https://newsapi.org/v1/articles?source='+source+'&apiKey='+this.key; 
 			}
-
+           
 			var self = this;
 
-			fetch(url, {
+			fetch(url, { //Cross-browser : if fetch doesnt exist, fallback to xhttp
 				method: 'get'
 			}).then(function(res){
 				return res.json();
 			}).then(function(res){
-				self.results.push(res.articles[i]);
-				//console.log(res.articles[i]); 
+				//console.log(res);
+				self.results.push(res);
 			});
+			
+
 
 			// Push latest data to idb-for a particular category
 			// Diferent datbase from sources, to be accessed when offline
-		},
-
-		setItem: function(key,item){  
-			store.setItem(key, item).then(function() {
-			    console.log('Key is cleared!');
-			}).catch(function(err) {
-			    console.log(err);
-			});
 		},
 
 		createImage: function(id, blob){
@@ -155,12 +231,12 @@ var app = new Vue({
 if (navigator.onLine) {
 	//Online Behavior 
 	app.toggleOnline();
-	// Fetch data online
+	// Fetch data online -populate with recent headlines based on idb
+	// if no idb sources then get all popular
 } else {
 	// ofline Behaviour
 	app.toggleOffline();
-
-    //Fetch data from indexedDB
+    //Fetch most recent data from indexedDB or saved bookmarks
 	//Links greyed out
 	//Conyent discolored
 }
@@ -176,6 +252,8 @@ self.addEventListener('offline', function(e) {
 
 
 
+//Features
+// Book mark to read later catchup section
 
 
 
